6dd1b38724ae679bb345a698af0838ee6d478702
AlbertFalzon <73096411+AlbertFalzon@users.noreply.github.com>
2021-07-21 10:11:27 +0200

    Reduced redundant elements (only 1 canvas and 1 TextMesh Pro per element. Made element blocks use the cpk hex attribute to colour the material. Added Basis to destroy spawned elements. Changed spawn script to first check the element in the list and spawn it only if it belongs in the current category.

--- Periodic Table Generator/Assets/Scripts/ElementsJsonDecoder.cs
+++ Periodic Table Generator/Assets/Scripts/ElementsJsonDecoder.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,9 +15,15 @@ public class ElementsJsonDecoder : MonoBehaviour
 
     // Object prefab to use on the periodic table
     [SerializeField] GameObject ElementPrefab;
+    [SerializeField] string CurrentCategory = "alkali metal";
+
+    // Frequently Referred Objects
+    Transform CanvasChild;
 
     IEnumerator Start()
     {
+        // Initialise CanvasChild
+        CanvasChild = transform.GetChild(0);
         // Populate object with their respective json file contents
         ElementsDetail = JsonUtility.FromJson<ElementsJsonDetails>(PeriodicTableJson.text);
         // elementsLookup = JsonUtils.ImportJson<ElementsJsonLookup>(periodicTableLookup);
@@ -25,24 +32,65 @@ public class ElementsJsonDecoder : MonoBehaviour
         for(int i = 0; i < ElementsDetail.Elements.Length; i++)
         {
             // print("Block Name: " + ElementsDetail.Elements[i].Name + "\nXpos: " + ElementsDetail.Elements[i].Xpos + "\nYpos: " + ElementsDetail.Elements[i].Ypos);
-            yield return StartCoroutine(SpawnElement(i));
+            yield return StartCoroutine(CheckElement(i));
+        }
+
+        yield return StartCoroutine(DestroyChildren());
+    }
+    
+    public IEnumerator CheckElement(int index)
+    {
+        Details ElementToCheck = ElementsDetail.Elements[index];
+
+        if(ElementToCheck.Category == CurrentCategory)
+        {
+            print("Spawning Element: " + ElementToCheck.Name);
+            yield return StartCoroutine(SpawnElement(ElementToCheck));
+        } else
+        {
+            print("Skipped Element: " + ElementToCheck.Name);
+            yield return new WaitForFixedUpdate();
         }
     }
-
-    public IEnumerator SpawnElement(int index)
+    
+    public IEnumerator SpawnElement(Details ElementToSpawn)
     {
-        // Spawn object and set the spawner as its parent
-        GameObject NextElement = Instantiate(ElementPrefab, new Vector3(ElementsDetail.Elements[index].Xpos, -ElementsDetail.Elements[index].Ypos, 0), transform.rotation);
-        NextElement.transform.SetParent(transform);
+        // Spawn object and set the canvas as its parent
+        GameObject NextElement = Instantiate(ElementPrefab, new Vector3(ElementToSpawn.Xpos, -ElementToSpawn.Ypos, 0), transform.rotation);
+        NextElement.transform.SetParent(CanvasChild);
 
-        // Get canvas child of spawned element
-        GameObject CanvasChild = NextElement.transform.GetChild(0).gameObject;
+        // Get TMP child of spawned element
+        GameObject TextChild = NextElement.transform.GetChild(0).gameObject;
 
-        // Set text of spawned element
-        CanvasChild.transform.GetChild(0).GetComponent<Text>().text = (ElementsDetail.Elements[index].Number).ToString();
-        CanvasChild.transform.GetChild(1).GetComponent<Text>().text = ElementsDetail.Elements[index].Symbol;
-        CanvasChild.transform.GetChild(2).GetComponent<Text>().text = ElementsDetail.Elements[index].Name;
-        CanvasChild.transform.GetChild(3).GetComponent<Text>().text = (ElementsDetail.Elements[index].Atomic_Mass).ToString();
+        // Set text in a string and assign it
+        string NewText = "<align=\"right\"><size=18>" + ElementToSpawn.Number + "</size></align>\n<size=40>" + ElementToSpawn.Symbol + "</size>\n<size=14>" + ElementToSpawn.Name + "\n" + ElementToSpawn.Atomic_Mass + "</size>";
+        TextChild.GetComponent<TextMeshProUGUI>().text = NewText;
+        Color NewColor;
+
+        if (ColorUtility.TryParseHtmlString("#" + ElementToSpawn.Cpk_Hex, out NewColor))
+        {
+            NextElement.GetComponent<MeshRenderer>().material.SetColor("_Color", NewColor);
+        }
+       
+
+        yield return new WaitForFixedUpdate();
+    }
+
+    public IEnumerator DestroyChildren()
+    {
+        int i = 0;
+        GameObject[] SpawnedElements = new GameObject[CanvasChild.childCount];
+
+        foreach(Transform element in CanvasChild)
+        {
+            SpawnedElements[i] = element.gameObject;
+            i += 1;
+        }
+        
+        foreach(GameObject element in SpawnedElements)
+        {
+            Destroy(element);
+        }
 
         yield return new WaitForFixedUpdate();
     }
